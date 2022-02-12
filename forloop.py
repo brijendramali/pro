1d76e8399b9866be71edfd367369445149ba5913
brijendramali <95962427+brijendramali@users.noreply.github.com>
2022-02-12 06:51:22 +0000

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,49 @@
+
+
+
+list1= ["Harry","Larry","carry","Marry"]
+
+for item in list1:
+	print(item)
+
+
+
+# for x in "banana":
+#   print(x)
+
+'''fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+	if x == "banana":
+	  break
+	print(x)'''
+
+# list1 = ["apple", "banana", "cherry"]
+# for j in list1:
+#   if j == "apple":
+#   	continue
+#   print(j)
+
+
+
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "apple":
+    break
+  print(x) '''
+
+
+# for i in range(1,500+1):
+#     if(i%8==0 and i%7==0):
+#         print("number devide by 8,7" ,i)
+
+
+#multipal number 
+# n=int(input("enter"))
+# p=int(input("Enter"))
+
+# def print_table(num):
+# 	for i in range(1,p+1):
+# 		print(num,'*',i,'=',num*i)
+
+# print_table(n)
